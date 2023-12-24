c179e54a4719c16a200024278ab8c8b1459e8d5b
rizan1998 <rizanalfalah@gmail.com>
2023-12-24 11:13:35 +0700

    menambah komentar dan tipe data number di dart

---
+++ belajar_dart_dasar/bin/comment.dart
@@ -0,0 +1,12 @@
+///this is main function
+///will executed by dart
+
+void main() {
+  // this is name variable
+  var name = 'Muhamad Rijan Alpalah';
+
+  /** 
+   * this is multiline command you can add more
+  */
+  print(name);
+}
---
+++ belajar_dart_dasar/bin/number.dart
@@ -0,0 +1,12 @@
+void main() {
+  int number1 = 10;
+  double number2 = 10.5;
+  var number = 10;
+
+  num number3 = 10.5;
+
+  print(number1);
+  print(number2);
+  print(number3);
+  print(number);
+}
